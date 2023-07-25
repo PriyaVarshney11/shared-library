7e81fe2cca9ca5334926117eeae0a02b1f761e36
PriyaVarshney11 <126467165+PriyaVarshney11@users.noreply.github.com>
2023-07-25 14:50:38 +0530

    Update terraform_ci.groovy

--- vars/terraform_ci.groovy
+++ vars/terraform_ci.groovy
@@ -28,10 +28,13 @@ pipeline {
         stage('Terraform CI') {
             steps {
                 script {
-                    def action = "init"
+                    //def action = "init"
                     //def command = "python3 terraform_ci.py ${action}"
-                    sh "python3 $WORKSPACE/resources/terraform_ci.py ${action}"
-                     
+                    //sh "python3 $WORKSPACE/resources/terraform_ci.py ${action}"
+                      def content = readFileFromResources('terraform_ci.py')
+                      println 'content'
+                      writeFile file: 'output.py' , text: content
+                      sh "python3 output.py init"
 
                 }
             }
