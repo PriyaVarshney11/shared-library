4a404f94b70752776ce6ebc1337a68f8e02348c1
PriyaVarshney11 <126467165+PriyaVarshney11@users.noreply.github.com>
2023-07-25 14:35:39 +0530

    Create terraform_ci.py

---
+++ resources/terraform_ci.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+
+class TerraformError(Exception):
+    pass
+
+def terraform_init():
+    try:
+        subprocess.run(["terraform", "init"], check=True)
+    except subprocess.CalledProcessError:
+        raise TerraformError("Failed to run 'terraform init'")
+
+def terraform_fmt():
+    try:
+        subprocess.run(["terraform", "fmt", "-recursive"], check=True)
+    except subprocess.CalledProcessError:
+        raise TerraformError("Failed to run 'terraform fmt'")
+
+def terraform_plan():
+    try:
+        subprocess.run(["terraform", "plan"], check=True)
+    except subprocess.CalledProcessError:
+        raise TerraformError("Failed to run 'terraform plan'")
+
+# Provide a way to execute the actions from the command line
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python terraform_ci.py <action>")
+        sys.exit(1)
+
+    action = sys.argv[1]
+    try:
+        if action == "init":
+            terraform_init()
+        elif action == "fmt":
+            terraform_fmt()
+        elif action == "plan":
+            terraform_plan()
+        else:
+            raise TerraformError(f"Unknown action: {action}")
+    except TerraformError as e:
+        print(str(e))
+        sys.exit(1)
