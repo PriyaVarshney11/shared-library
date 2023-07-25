adca6dca45b35a5aa8fede300dea434d87c89275
PriyaVarshney11 <126467165+PriyaVarshney11@users.noreply.github.com>
2023-07-25 13:50:11 +0530

    Update terraform_ci.py

--- vars/terraform_ci.py
+++ vars/terraform_ci.py
@@ -1,52 +1,46 @@
+# terraform_ci.py
+
 import subprocess
 import sys
+
+class TerraformError(Exception):
+    pass
 
 def terraform_init():
     try:
         subprocess.run(["terraform", "init"], check=True)
     except subprocess.CalledProcessError:
-        print("Failed to run 'terraform init'")
-        sys.exit(1)
+        raise TerraformError("Failed to run 'terraform init'")
 
 def terraform_fmt():
     try:
         subprocess.run(["terraform", "fmt", "-recursive"], check=True)
     except subprocess.CalledProcessError:
-        print("Failed to run 'terraform fmt'")
-        sys.exit(1)
-
-
-
-
-
-def terraform_validate():
-    try:
-        subprocess.run(["terraform", "validate"], check=True)
-    except subprocess.CalledProcessError:
-        print("Failed to run 'terraform validate'")
-        sys.exit(1)
+        raise TerraformError("Failed to run 'terraform fmt'")
 
 def terraform_plan():
     try:
         subprocess.run(["terraform", "plan"], check=True)
     except subprocess.CalledProcessError:
-        print("Failed to run 'terraform plan'")
-        sys.exit(1)
+        raise TerraformError("Failed to run 'terraform plan'")
+
+# Provide a way to execute the actions from the command line
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 terraform_ci.py <action>")
+        print("Usage: python terraform_ci.py <action>")
         sys.exit(1)
 
     action = sys.argv[1]
-    if action == "init":
-        terraform_init()
-    elif action == "fmt":
-        terraform_fmt()
-    elif action == "validate":
-        terraform_validate()
-    elif action == "plan":
-        terraform_plan()
-    else:
-        print(f"Unknown action: {action}")
+    try:
+        if action == "init":
+            terraform_init()
+        elif action == "fmt":
+            terraform_fmt()
+        elif action == "plan":
+            terraform_plan()
+        else:
+            raise TerraformError(f"Unknown action: {action}")
+    except TerraformError as e:
+        print(str(e))
         sys.exit(1)
